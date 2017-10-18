f46fd00ee4268e9d76765a57088c77987c021bdd
Hamza Anis <hamzaanis9514@gmail.com>
2017-10-19 00:09:51 +0500

    Posting ad and some exceptions

--- Java/org.bst.gumtree/src/org/bst/gumtree/GumTreeInputController.java
+++ Java/org.bst.gumtree/src/org/bst/gumtree/GumTreeInputController.java
@@ -3,9 +3,29 @@ package org.bst.gumtree;
 import java.nio.file.Paths;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class GumTreeInputController implements ControlledScreen {
+
+	@FXML
+	TextField txtfieldCategory;
+	@FXML
+	TextField txtfieldLocation;
+	@FXML
+	TextField txtfieldTitle;
+	@FXML
+	TextField txtfieldYoutube;
+	@FXML
+	TextField txtfieldDiscription;
+	@FXML
+	TextField txtfieldPrice;
+	@FXML
+	TextField txtfieldPhone;
+	@FXML
+	Button buttonPost;
 
 	ScreensController myScreenController;
 	Main myLogicalParent;
@@ -21,5 +41,33 @@ public class GumTreeInputController implements ControlledScreen {
 		System.out.println(Paths.get(".").toAbsolutePath().normalize().toString());
 		System.out.println("Send");
 	}
-	
+
+	@FXML
+	protected void postAd() {
+		System.out.println("Calling");
+		if (txtfieldTitle.getText().equals("") || txtfieldCategory.getText().equals("")
+				|| txtfieldPrice.getText().equals("") || txtfieldDiscription.getText().equals("")
+				|| txtfieldPhone.getText().equals("") || txtfieldPrice.getText().equals("")) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error Dialog");
+			alert.setHeaderText("Enter required fields");
+			alert.setContentText("Please enter all details that are necesary");
+			alert.showAndWait();
+			return;
+		}
+		String s = txtfieldDiscription.getText();
+		String[] words = s.trim().split("\\s+");
+		if (words.length <= 12) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Less Description");
+			alert.setHeaderText("Description Field");
+			alert.setContentText("12 words minimum.");
+			alert.showAndWait();
+			return;
+		}
+		myLogicalParent.postADD(txtfieldCategory.getText(), txtfieldLocation.getText(), txtfieldTitle.getText(),
+				txtfieldYoutube.getText(), txtfieldDiscription.getText(), txtfieldPrice.getText(),
+				txtfieldPhone.getText());
+	}
+
 }
--- Java/org.bst.gumtree/src/org/bst/gumtree/Main.java
+++ Java/org.bst.gumtree/src/org/bst/gumtree/Main.java
@@ -15,6 +15,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	WebDriver driver;
 	ScreensController mainContainer;
+	boolean loggedIn = false;
+	boolean started = false;
 
 	public Main() {
 		mainContainer = new ScreensController(this);
@@ -101,7 +103,13 @@ public class Main extends Application {
 				System.out.println("Login Gumtree");
 				driver.navigate().to("https://my.gumtree.com/login");
 				// Google login
-				driver.findElement(By.xpath("//*[@id=\"login-form\"]/div/button")).click();
+				try {
+					driver.findElement(By.xpath("//*[@id=\"login-form\"]/div/button")).click();
+					loggedIn = true;
+				} catch (Exception e) {
+					loggedIn = true;
+					System.out.println("Already Logged in");
+				}
 				// driver.navigate().to("https://google.com");
 			}
 		}).start();
@@ -109,68 +117,142 @@ public class Main extends Application {
 
 	public void postADD(String category, String Location, String Title, String youtube, String Description,
 			String price, String Phone) {
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				// Post ad
-				driver.findElement(By.xpath("/html/body/div[2]/div/main/section/div/div/a")).click();
+		if (!started) {
+			started = true;
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
 
-				try {
-					Thread.sleep(2000);
-					driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/input[2]")).click();
-				} catch (Exception e) {
-					System.out.println("No draft");
+					while (true) {
+						System.out.println("Checking logging in");
+						if (loggedIn) {
+							System.out.println("It is logged in");
+							new Thread(new Runnable() {
+								@Override
+								public void run() {
+									boolean notFound = true;
+
+									// Post ad
+									driver.findElement(By.xpath("/html/body/div[2]/div/main/section/div/div/a"))
+											.click();
+
+									try {
+										Thread.sleep(2000);
+										driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/input[2]"))
+												.click();
+										System.out.println("Draft handled");
+									} catch (Exception e) {
+										System.out.println("No draft");
+									}
+									while (notFound) {
+										try {
+											// category
+											driver.findElement(By.xpath("//*[@id=\"post-ad_title-suggestion\"]"))
+													.sendKeys(category);
+											notFound = false;
+											System.out.println("Category is displayed");
+										} catch (Exception e) {
+											// TODO: handle exception
+										}
+									}
+									notFound = true;// will be used later
+
+									// pressing enter
+									driver.findElement(By.xpath("//*[@id=\"post-ad_title-suggestion\"]"))
+											.sendKeys(Keys.ENTER);
+									try {
+										Thread.sleep(1000);
+									} catch (Exception e) {
+									}
+									driver.findElement(By.xpath("//*[@id=\"post-ad_title-suggestion\"]"))
+											.sendKeys(Keys.ENTER);
+
+									try {
+										Thread.sleep(2000);
+									} catch (InterruptedException e) {
+										System.out.println("Thread sleep error");
+										e.printStackTrace();
+									}
+
+									while (notFound) {
+										try {
+											// location
+											driver.findElement(By.xpath("//*[@id=\"post-ad_postcode\"]")).clear();
+											driver.findElement(By.xpath("//*[@id=\"post-ad_postcode\"]"))
+													.sendKeys(Location);
+											notFound = false;
+											System.out.println("Location is displayed");
+										} catch (Exception e) {
+											System.out.println("Location exception");
+											try {
+												driver.findElement(By
+														.xpath("//*[@id=\"post-ad-container\"]/div[2]/div[1]/div/div[1]/div[1]/button"))
+														.click();
+											} catch (Exception e) {
+												System.out.println("Location exception 1");
+
+											}
+										}
+									}
+									notFound = true;
+									driver.findElement(By.xpath("//*[@id=\"post-ad_postcode\"]")).sendKeys(Keys.ENTER);
+									try {
+										Thread.sleep(2000);
+									} catch (InterruptedException e) {
+										System.out.println("Thread sleep error");
+										e.printStackTrace();
+									}
+
+									while (notFound) {
+										try {
+											// title
+											driver.findElement(By.xpath("//*[@id=\"post-ad_title\"]")).sendKeys(Title);
+											notFound = false;
+											System.out.println("Title is displayed");
+										} catch (Exception e) {
+											System.out.println("Title Exception");
+										}
+									}
+
+									// youtube link
+									driver.findElement(By.xpath("//*[@id=\"post-ad_youtube\"]")).sendKeys(youtube);
+
+									// description
+									driver.findElement(By.xpath("//*[@id=\"description\"]")).sendKeys(Description);
+
+									// price
+									driver.findElement(By.xpath("//*[@id=\"price\"]")).sendKeys(price);
+
+									// radio button
+									driver.findElement(By
+											.xpath("//*[@id=\"post-ad-container\"]/div[11]/div/div[2]/div[1]/div/div[1]/label"))
+											.click();
+									// phone number
+									driver.findElement(By.xpath("//*[@id=\"post-ad_contactTelephone\"]"))
+											.sendKeys(Phone);
+									// post ad button
+									driver.findElement(By.xpath("//*[@id=\"submit-button-2\"]")).click();
+
+									try {
+										Thread.sleep(2000);
+									} catch (InterruptedException e) {
+										System.out.println("Thread sleep error");
+										e.printStackTrace();
+									}
+									driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/main/div[1]/a[1]"))
+											.click();
+									// conformation
+
+								}
+							}).start();
+							break;
+						} else {
+							System.out.println("Injecting started");
+						}
+					}
 				}
-				// category
-				driver.findElement(By.xpath("//*[@id=\"post-ad_title-suggestion\"]")).sendKeys(category);
-				// pressing enter
-				driver.findElement(By.xpath("//*[@id=\"post-ad_title-suggestion\"]")).sendKeys(Keys.ENTER);
-				try {
-					Thread.sleep(1000);
-					driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/input[2]")).click();
-				} catch (Exception e) {
-					System.out.println("No draft");
-				}
-				driver.findElement(By.xpath("//*[@id=\"post-ad_title-suggestion\"]")).sendKeys(Keys.ENTER);
-
-				try {
-					Thread.sleep(2000);
-				} catch (InterruptedException e) {
-					System.out.println("Thread sleep error");
-					e.printStackTrace();
-				}
-				// location
-				driver.findElement(By.xpath("//*[@id=\"post-ad_postcode\"]")).sendKeys(Location);
-				driver.findElement(By.xpath("//*[@id=\"post-ad_postcode\"]")).sendKeys(Keys.ENTER);
-				try {
-					Thread.sleep(2000);
-				} catch (InterruptedException e) {
-					System.out.println("Thread sleep error");
-					e.printStackTrace();
-				}
-				// title
-				driver.findElement(By.xpath("//*[@id=\"post-ad_title\"]")).sendKeys(Title);
-
-				// youtube link
-				driver.findElement(By.xpath("//*[@id=\"post-ad_youtube\"]")).sendKeys(youtube);
-
-				// description
-				driver.findElement(By.xpath("//*[@id=\"description\"]")).sendKeys(Description);
-
-				// price
-				driver.findElement(By.xpath("//*[@id=\"price\"]")).sendKeys(price);
-
-				// radio button
-				driver.findElement(
-						By.xpath("//*[@id=\"post-ad-container\"]/div[11]/div/div[2]/div[1]/div/div[1]/label")).click();
-				// phone number
-				driver.findElement(By.xpath("//*[@id=\"price\"]")).sendKeys(price);
-
-				// conformation /html/body/div[2]/div/div[3]/main/div[1]/a[1]
-
-			}
-		}).start();
-
+			}).start();
+		}
 	}
 
 	public void loginGoogle(String username, String Password) {
