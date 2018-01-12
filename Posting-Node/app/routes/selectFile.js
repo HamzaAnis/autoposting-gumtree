9502bff144ea32bb3a5d484accd6e4342712ec62
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-12 15:20:12 -0800

    To process new upload

--- Posting-Node/app/routes/selectFile.js
+++ Posting-Node/app/routes/selectFile.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/selectFile', function(req,res) {
+router.get('/selectFile/:filename', function(req,res) {
     console.log("I am here");
+    console.log(req.params.filename);
 });
 module.exports = router;
