22b84ecd48aa634a5594ef981a95641605a0669f
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-12 11:21:22 -0800

    New folder and views

--- Posting-Node/app.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var reload = require('reload');
-var app = express();
-
-app.set('port',process)
---
+++ Posting-Node/app/app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var reload = require('reload');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
