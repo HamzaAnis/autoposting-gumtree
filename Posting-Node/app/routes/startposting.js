8bf6924b37695a61b06a9b714ac204b6db50a61e
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-14 22:57:04 +0500

    Webdriver example

--- Posting-Node/app/routes/startposting.js
+++ Posting-Node/app/routes/startposting.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/startposting', function(req, res) {
-    
+router.get('/startposting', function (req, res) {
+    var webdriverio = require('webdriverio');
+    var options = {
+        desiredCapabilities: {
+            browserName: 'chrome'
+        }
+    };
+    var client = webdriverio.remote(options);
+
+
+    client
+        .init()
+        .url('https://duckduckgo.com/')
+        .setValue('#search_form_input_homepage', 'WebdriverIO')
+        .click('#search_button_homepage')
+        .getTitle().then(function (title) {
+            console.log('Title is: ' + title);
+            // outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
+        })
+        .end();
 });
+
+
 
 module.exports = router;
